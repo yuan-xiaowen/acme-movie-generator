ffee3d69c22350310732a944c154c8e2b8b64b6b
ericpkatz <ericpkatz@gmail.com>
2022-07-16 06:48:39 -0400

    commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+app.use((err, req, res, next)=> {
+  console.log(err);
+  res.status(err.status || 500).send({ err });
+});
+
+
+
+const init = async()=> {
+  try {
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=> console.log(`listening on port ${port}`));
+  }
+  catch(ex){
+    console.log(ex);
+  }
+}
+
+init();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+import React, { Component } from 'react';
+
+
+const root = createRoot(document.querySelector('#root'));
+root.render(<hr />);
